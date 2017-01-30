1243483eccac8bfe2c549abf4ca31d2778d132ae
Rahul Bangar <bangar1991@gmail.com>
2017-01-29 20:41:58 -0700

    First implementation of Register wire format

--- a1/src/cs455/overlay/wireformats/Register.java
+++ a1/src/cs455/overlay/wireformats/Register.java
@@ -1,11 +1,40 @@
 package cs455.overlay.wireformats;
 
+import java.io.*;
+
 public class Register implements Event 
 {
+	private String ipAddress;
+	private int portnum;
 	
+	public Register(String ip, int port)
+	{
+		ipAddress = ip;
+		portnum = port;
+	}
+	
+	@Override
 	public EventType getType()
 	{
 		return EventType.REGISTER_REQUEST;
 	}
 	
+	@Override
+	public byte[] getBytes() throws IOException
+	{
+		byte[] bytes = null;
+		
+		try(ByteArrayOutputStream bout = new ByteArrayOutputStream();
+			DataOutputStream dout = new DataOutputStream(new BufferedOutputStream(bout)))
+		{
+			dout.writeInt(EventType.REGISTER_REQUEST.ordinal());
+			dout.writeUTF(ipAddress);
+			dout.writeInt(portnum);
+			dout.flush();
+			
+			bytes = bout.toByteArray();
+		}
+		
+		return bytes;
+	}
 }
