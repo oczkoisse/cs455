844158e969756a8edf664042473454ee37686f13
Rahul Bangar <bangar1991@gmail.com>
2017-02-23 18:28:04 -0700

    Impemented generic blocking queue

---
+++ a2/src/cs455/scaling/server/BlockingQueue.java
@@ -0,0 +1,47 @@
+package cs455.scaling.server;
+
+import java.util.*;
+
+public class BlockingQueue<T> {
+	
+	private ArrayDeque<T> q;
+	
+	public BlockingQueue()
+	{
+		q = new ArrayDeque<T>();
+	}
+	
+	public boolean enqueue(T elem)
+	{
+		boolean success = false;
+		synchronized(q)
+		{
+			success = q.offer(elem);
+			if (success)
+			{
+				q.notify();
+			}
+		}
+		return success;
+	}
+	
+	public T dequeue()
+	{
+		synchronized(q)
+		{
+			while(q.isEmpty())
+			{
+				try
+				{
+					q.wait();
+				}
+				catch(InterruptedException e)
+				{
+					break;
+				}
+			}
+			return q.poll();
+		}
+	}
+
+}
