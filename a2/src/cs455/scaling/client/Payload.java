9abe8c8be95bb356defb47b8321102714037d121
Rahul Bangar <bangar1991@gmail.com>
2017-03-05 18:53:48 -0700

    Added docs for Payload class

--- a2/src/cs455/scaling/client/Payload.java
+++ a2/src/cs455/scaling/client/Payload.java
@@ -5,20 +5,35 @@ import java.nio.ByteBuffer;
 
 import cs455.scaling.util.Hasher;
 
-// Not thread safe
+/**
+ * Abstraction for the payload data.
+ * Meant to be used by {@link cs455.scaling.client.Client}.
+ * @author Rahul Bangar
+ *
+ */
 public class Payload {
 	
+	/**
+	 * Internal storage for the payload data
+	 */
 	private byte[] data;
 
+	/**
+	 * Random number generator to refresh the payload data
+	 */
 	private final Random rng;
 	
+	/**
+	 * The hash of the data in the form of hexadecimal string
+	 * Internally computed using {@link cs455.scaling.util.Hasher}
+	 */
 	private String hashString;
 	
 	
 	/**
 	 * Initializes the payload with random data of specified size
-	 * @param size The size of the payload
-	 * @param u The unit of specified size (bytes, kilobytes)
+	 * @param size 	the size of the payload
+	 * @param u 	the unit of specified size (bytes, kilobytes)
 	 */
 	public Payload(int size, Unit u)
 	{
@@ -42,19 +57,24 @@ public class Payload {
 		
 	}
 	
+	/**
+	 * Recomputes the hash of the payload data
+	 */
 	private void updateHashString()
 	{
 		this.hashString = Hasher.hashAsString(ByteBuffer.wrap(this.data));
 	}
 	
+	/**
+	 * Refreshes the internal payload data with new random data
+	 */
 	private void updateData()
 	{
 		rng.nextBytes(this.data);
 	}
 	
 	/**
-	 * Refreshes the data with new random data
-	 * Also, recomputes the hash
+	 * Refreshes the data with new random data and recomputes the hash.
 	 */
 	public void refresh()
 	{
@@ -72,14 +92,20 @@ public class Payload {
 	}
 	
 	/**
-	 * Returns a read only form of payload data
-	 * @return
+	 * Gets the read only form of input payload data
+	 * @return Read-only form of input payload data
 	 */
 	public ByteBuffer getData()
 	{
 		return ByteBuffer.wrap(data).asReadOnlyBuffer();
 	}
 	
+	/**
+	 * Unit of payload data
+	 * Can be bytes or kilo-bytes
+	 * @author Rahul Bangar
+	 *
+	 */
 	public enum Unit {
 		BYTES,
 		K_BYTES;
